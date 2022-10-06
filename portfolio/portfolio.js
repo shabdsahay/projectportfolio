30c002b0d2717898b2242876c598ab96fb49883d
shabdsahay <113663298+shabdsahay@users.noreply.github.com>
2022-10-07 00:04:26 +0530

    Add files via upload

---
+++ portfolio/portfolio.js
@@ -0,0 +1,26 @@
+/*navigation bar */
+function myFunction() {
+    var x = document.getElementById("myheadnav");
+    if (x.className === "headnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "headnav";
+    }
+  } 
+/*end of navbar */
+/*home*/ 
+function NewTab() {
+    window.open("https://www.hackerrank.com/SHABDSAHAY_MDB?hr_r=1");
+}
+
+/*end of home*/
+
+function func() {
+  window.open("https://www.hackerrank.com/SHABDSAHAY_MDB");
+}
+function funct() {
+  window.open("https://github.com/shabdsahay");
+}
+function info(){
+  alert("ph. number-> 7701966865 \n email-> shabd2112036@akgec.ac.in \n instagram- @sahay_shabd")
+}
